e3241b97c6b3766bf69fbefece7d3c194cd07f44
leeyoonsu <101182523+leey00nsu@users.noreply.github.com>
2022-05-07 06:21:49 +0900

    [Bronze III] Title: 팩토리얼, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int factorial(int n) {
+  if(n>0)
+    return n*factorial(n-1);
+  else
+    return 1;
+}
+int main() {
+    int n,result;
+    scanf("%d",&n);
+    result=factorial(n);
+    printf("%d",result);
+    return 0;
+}
+  
