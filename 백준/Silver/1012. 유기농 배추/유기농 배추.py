9ed677467c6bb0623df1d5b64c080ba73d461b95
leeyoonsu <101182523+leey00nsu@users.noreply.github.com>
2023-02-05 23:52:00 +0900

    Update 유기농 배추.py

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,38 +1,40 @@
-import sys
-sys.setrecursionlimit(10000) 
+# import sys
+# sys.setrecursionlimit(10000) 
 
-T = int(sys.stdin.readline()) 
+# T = int(sys.stdin.readline()) 
 
-dx = [-1, 1, 0, 0] 
-dy = [0, 0, -1, 1]
+# dx = [-1, 1, 0, 0] 
+# dy = [0, 0, -1, 1]
 
-def dfs(x, y):
-  if x <= -1 or x >= N or y<= -1 or y>= M:
-    return False
+# def dfs(x, y):
+#   if x <= -1 or x >= N or y<= -1 or y>= M:
+#     return False
 
-  if graph[x][y] == 1:
-    graph[x][y] = 0
+#   if graph[x][y] == 1:
+#     graph[x][y] = 0
 
-    for i in range(4):
-      dfs(x + dx[i], y+dy[i])
+#     for i in range(4):
+#       dfs(x + dx[i], y+dy[i])
 
-    return True
-  return False
+#     return True
+#   return False
 
-answer = []
-for _ in range(T):
-  M, N, K = list(map(int, sys.stdin.readline().split()))  
+# answer = []
+# for _ in range(T):
+#   M, N, K = list(map(int, sys.stdin.readline().split()))  
 
-  graph = [[0]*M for _ in range(N)]
+#   graph = [[0]*M for _ in range(N)]
 
-  for _ in range(K):
-    x, y = map(int, input().split())
-    graph[y][x] = 1
+#   for _ in range(K):
+#     x, y = map(int, input().split())
+#     graph[y][x] = 1
 
-  cnt = 0
-  for i in range(N):
-    for j in range(M):
-      if dfs(i, j):
-        cnt +=1
+#   cnt = 0
+#   for i in range(N):
+#     for j in range(M):
+#       if dfs(i, j):
+#         cnt +=1
 
-  print(cnt)
+#   print(cnt)
+
+코드 추가 
