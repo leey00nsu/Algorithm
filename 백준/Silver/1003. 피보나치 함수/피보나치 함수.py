5bfde736e5a3f2993eaaeaddc9bbf94bcfe5077a
leeyoonsu <101182523+leey00nsu@users.noreply.github.com>
2023-02-15 21:44:03 +0900

    [Silver III] Title: 피보나치 함수, Time: 136 ms, Memory: 115004 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,24 @@
+def fib(n):
+  if(n==0):
+    return [1,0]
+  elif(n==1):
+    return [0,1]
+
+  dp=[0]*(n+1)
+  dp[0]=[1,0]
+  dp[1]=[0,1]
+  for i in range(2,len(dp)):
+
+    dp[i] = [x+y for x,y in zip(dp[i-2],dp[i-1])]
+
+  return dp[n]
+
+
+
+
+T = int(input())
+
+for i in range(T):
+  N = int(input())
+  zero,one = fib(N)
+  print(zero,one)
