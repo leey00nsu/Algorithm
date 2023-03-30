2b4b0d1172c1416e8d3ba427955fa044c773f066
leeyoonsu <101182523+leey00nsu@users.noreply.github.com>
2023-03-30 16:15:06 +0900

    [level 2] Title: 구명보트, Time: 19.84 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,106 +1,27 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.prev = null;
-    this.next = null;
-  }
-}
-
-class DoubleLinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-
-  push(val) {
-    let new_node = new Node(val);
-    if (!this.head) {
-      this.head = new_node;
-      this.tail = new_node;
-    } else {
-      this.tail.next = new_node;
-      new_node.prev = this.tail;
-      this.tail = new_node;
-    }
-    this.length += 1;
-    return new_node;
-  }
-
-  pop() {
-    let current = this.tail;
-
-    if (!current) return null;
-    if (current === this.head) {
-      this.tail = null;
-      this.head = null;
-    } else {
-      this.tail = current.prev;
-      this.tail.next = null;
-      current.prev = null;
-    }
-    this.length -= 1;
-    return current.val;
-  }
-
-  shift() {
-    let current = this.head;
-    if (!current) return null;
-    if (current === this.tail) {
-      this.tail = null;
-      this.head = null;
-    } else {
-      this.head = current.next;
-      this.head.prev = null;
-      current.next = null;
-    }
-
-    this.length -= 1;
-    return current.val;
-  }
-
-  unshift(val) {
-    let new_node = new Node(val);
-    if (this.length === 0) {
-      this.head = new_node;
-      this.tail = this.head;
-    } else {
-      this.head.prev = new_node;
-      new_node.next = this.head;
-      this.head = new_node;
-    }
-
-    this.length += 1;
-
-    return new_node;
-  }
-}
-
 function getBoats(people, limit) {
   let boats = 0;
+  let i = 0;
+  let j = people.length - 1;
   people.sort((a, b) => b - a);
 
-  let deque = new DoubleLinkedList();
-  for (let i = 0; i < people.length; i++) {
-    deque.push(people[i]);
-  }
-
   let curWeight = 0;
-  while (deque.length) {
+  while (i <= j) {
+    curWeight = 0;
     boats += 1;
-    let max_pop = deque.shift();
 
-    curWeight += max_pop;
+    let max = people[i];
+    i += 1;
+    curWeight += max;
 
-    while (deque.length) {
-      let min_pop = deque.pop();
-      curWeight += min_pop;
+    while (true) {
+      let min = people[j];
+
+      curWeight += min;
 
       if (curWeight > limit) {
-        deque.push(min_pop);
-        curWeight = 0;
         break;
       }
+      j -= 1;
     }
   }
 
