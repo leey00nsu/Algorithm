6a0273047ef0deeb51927393c3901661a7334a41
leeyoonsu <101182523+leey00nsu@users.noreply.github.com>
2023-07-10 13:57:44 +0900

    [level 2] Title: 2 x n 타일링, Time: 4.61 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  let dp = [];
+  for (let i = 0; i <= n; i++) {
+    dp.push(0);
+  }
+  dp[0] = 1;
+  dp[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    dp[i] = (dp[i - 2] + dp[i - 1])%1000000007;
+  }
+
+  return dp[n];
+}
