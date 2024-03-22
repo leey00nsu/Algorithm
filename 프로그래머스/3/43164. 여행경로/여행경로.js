7830c21c3147acbaaa287cbffbd2e8fd284326cb
leeyoonsu <101182523+leey00nsu@users.noreply.github.com>
2024-03-22 17:12:40 +0900

    [level 3] Title: 여행경로, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -0,0 +1,30 @@
+/*
+ICN 공항에서 출발하는 여행 경로를 짠다.
+모든 공항을 방문해야 한다.
+*/
+
+function solution(tickets) {
+    const candidates = []
+    const routeLength = tickets.length + 1
+    
+    tickets.sort()
+
+    function dfs(current,leftTickets) {
+        if(current.length === routeLength) {
+            return candidates.push(current)
+        } 
+        
+        const lastAirport = current.at(-1)
+        
+        leftTickets.forEach((ticket,idx) => {
+            if(ticket[0] === lastAirport) {
+                dfs([...current,ticket[1]],[...leftTickets.slice(0,idx),...leftTickets.slice(idx+1)])
+            }
+        })
+    }
+    
+    dfs(['ICN'],tickets)
+    
+    
+    return candidates[0];
+}
