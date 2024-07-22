09aa178cf9b08c8d613cf3820877962e3ec20e26
leeyoonsu <101182523+leey00nsu@users.noreply.github.com>
2024-07-22 12:14:37 +0900

    [goormlevel] Title: 하늘다리, Time: 0.10 ms, Memory: 40.11 MB, Difficulty: 3 -BaekjoonHub

--- goormlevel/226812/1. 하늘다리/하늘다리.js
+++ goormlevel/226812/1. 하늘다리/하늘다리.js
@@ -1,32 +1,40 @@
+// Run by Node.js
 const readline = require('readline');
 (async () => {
     let rl = readline.createInterface({ input: process.stdin });
-    
+
     const inputs = []
-    
+
     for await (const line of rl) {
         inputs.push(line.trim())
     }
-    
+
     const n = Number(inputs.shift())
-    
-    const heights = inputs.shift().split(' ').map(Number)
-    
+
+    const arr = inputs.shift().split(' ').map(Number)
+
     const stack = []
-    let count = 0
-    
-    for (let i = 0; i < n; i++) {
-			
-        while (stack.length > 0 && heights[stack[stack.length - 1]] <= heights[i]) {
-            if (heights[stack[stack.length - 1]] === heights[i]) {
-                count++
+    let cnt = 0
+
+    while(arr.length > 0) {
+        const current = arr.pop()
+
+        if(stack.length === 0) {
+            stack.push(current)
+        } else {
+            while(stack.at(-1) < current) {
+                stack.pop()
             }
-            stack.pop()
+
+            if(stack.at(-1) === current) {
+                cnt += 1
+            } else {
+                stack.push(current)
+            }
         }
-        stack.push(i)
     }
-    
-    console.log(count)
-    
+
+    console.log(cnt)
+
     process.exit();
 })();
