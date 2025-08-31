b7344470c434dc86539b1ea90889622dbc57857c
leeyoonsu <101182523+leey00nsu@users.noreply.github.com>
2025-08-31 14:22:12 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 152 ms, Memory: 9652 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const N = Number(input[0]);
+
+const dp = new Array(N + 1).fill(0);
+
+dp[0] = 0;
+dp[1] = 1;
+dp[2] = 3;
+
+for (let i = 3; i <= N; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 2]) % 10007;
+}
+
+console.log(dp[N]);
