c238287e8d68a003f603919c61781b7ba38b8815
leeyoonsu <101182523+leey00nsu@users.noreply.github.com>
2025-11-17 18:03:52 +0900

    [Gold V] Title: 평범한 배낭, Time: 216 ms, Memory: 10652 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
@@ -1,32 +1,23 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-let inputCursor = 0;
-
-const [N, K] = input[inputCursor++].split(" ").map(Number);
+const [N, K] = input[0].split(" ").map(Number);
 
 const items = [];
+const dp = Array(K + 1).fill(0);
 
-const dp = Array(N + 1)
-  .fill(0)
-  .map(() => Array(K + 1).fill(0));
+for (let i = 1; i <= N; i++) {
+  const [W, V] = input[i].split(" ").map(Number);
 
-for (let i = 0; i < N; i++) {
-  const [w, v] = input[inputCursor++].split(" ").map(Number);
-
-  items.push([w, v]);
+  items.push([W, V]);
 }
 
 for (let i = 1; i <= N; i++) {
-  const [w, v] = items[i - 1];
+  const [W, V] = items[i - 1];
 
-  for (let j = 1; j <= K; j++) {
-    dp[i][j] = dp[i - 1][j];
-
-    if (j >= w) {
-      dp[i][j] = Math.max(dp[i - 1][j - w] + v, dp[i][j]);
-    }
+  for (let j = K; j >= W; j--) {
+    dp[j] = Math.max(dp[j], dp[j - W] + V);
   }
 }
 
-console.log(dp[N][K]);
+console.log(dp[K]);
